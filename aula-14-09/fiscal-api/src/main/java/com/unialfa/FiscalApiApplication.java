f5b76cceaf02e7a9f4db91eb8605b359f297da09
luisgustavo <luis.gustav0@live.com>
2019-09-14 19:08:32 -0300

    primeiro commit

--- aula-14-09/fiscal-api/src/main/java/com/unialfa/FiscalApiApplication.java
+++ aula-14-09/fiscal-api/src/main/java/com/unialfa/FiscalApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FiscalApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FiscalApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(FiscalApiApplication.class, args);
+  }
 }
+
